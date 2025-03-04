7b72e647fd5f8fc030bb16c44d5837042390013b
Pau <p.core.dev@outlook.com>
2025-03-04 12:33:57 +0100

    Updated example

--- src/examples/example1/main.cpp
+++ src/examples/example1/main.cpp
@@ -232,9 +232,11 @@
 
 std::shared_ptr<Entity> earth;
 
+std::shared_ptr<Entity> box;
+
 std::shared_ptr<Entity> entity5;
 
-std::vector<std::shared_ptr<Entity>> cubes;
+//std::vector<std::shared_ptr<Entity>> cubes;
 
 std::shared_ptr<Entity> point_light;
 
@@ -302,7 +304,7 @@ void OpenGLEngine::Prepare(){
   point_light->AddMeshComponent();
   point_light->AddRenderComponent();
   point_light->AddTransformComponent();
-  point_light->GetTransformComponent()->SetPosition(glm::vec3(0.0f, 10.0f, -40.0f));
+  point_light->GetTransformComponent()->SetPosition(glm::vec3(0.0f, 2.0f, 0.0f));
   point_light->GetTransformComponent()->SetScale(glm::vec3(1.0f, 1.0f, 1.0f));
   point_light->AddMaterialComponent();
   point_light->GetMeshComponent()->LoadOBJ("../../data/models/sphere2.obj");
@@ -323,8 +325,8 @@ void OpenGLEngine::Prepare(){
   point_light->GetLightComponent()->SetAmbient(glm::vec3(0.2f));
   point_light->GetLightComponent()->SetDiffuse(glm::vec3(0.5f));
   point_light->GetLightComponent()->SetSpecular(glm::vec3(1.0f));
-  point_light->GetLightComponent()->SetConstant(1.0f);
-  point_light->GetLightComponent()->SetLinear(0.09f);
+  point_light->GetLightComponent()->SetConstant(0.3f);
+  point_light->GetLightComponent()->SetLinear(0.02f);
   point_light->GetLightComponent()->SetQuadratic(0.0f);
   point_light->GetLightComponent()->SetLightColor(glm::vec3(0.0f, 0.0f, 1.0f));
 
@@ -334,7 +336,7 @@ void OpenGLEngine::Prepare(){
   spot_light->AddMeshComponent();
   spot_light->AddRenderComponent();
   spot_light->AddTransformComponent();
-  spot_light->GetTransformComponent()->SetPosition(glm::vec3(0.0f, 30.0f, 40.0f));
+  spot_light->GetTransformComponent()->SetPosition(glm::vec3(60.0f, 20.0f, 0.0f));
   spot_light->GetTransformComponent()->SetScale(glm::vec3(1.0f, 1.0f, 1.0f));
   spot_light->AddMaterialComponent();
   spot_light->GetMeshComponent()->LoadOBJ("../../data/models/sphere1.obj");
@@ -356,15 +358,32 @@ void OpenGLEngine::Prepare(){
   spot_light->GetLightComponent()->SetDiffuse(glm::vec3(0.5f));
   spot_light->GetLightComponent()->SetSpecular(glm::vec3(1.0f));
   spot_light->GetLightComponent()->SetConstant(1.0f);
-  spot_light->GetLightComponent()->SetLinear(0.0f);
+  spot_light->GetLightComponent()->SetLinear(0.007f);
   spot_light->GetLightComponent()->SetQuadratic(0.0f);
-  spot_light->GetLightComponent()->SetCutOff(0.7f);
-  spot_light->GetLightComponent()->SetOuterCutOff(0.1f);
+  spot_light->GetLightComponent()->SetCutOff(1.0f);
+  spot_light->GetLightComponent()->SetOuterCutOff(0.0f);
   spot_light->GetLightComponent()->SetLightColor(glm::vec3(1.0f, 0.0f, 0.0f));
 
 
+  box = OpenGLEngine::Engine::Core::entity_manager_->CreateEntity();
+  box->AddMeshComponent();
+  box->AddRenderComponent();
+  box->AddTransformComponent();
+  box->GetTransformComponent()->SetPosition(glm::vec3(0.0f, 10.0f, 0.0f));
+  box->GetTransformComponent()->SetScale(glm::vec3(3.0f, 3.0f, 3.0f));
+  box->GetMeshComponent()->Cube();
+  box->AddMaterialComponent();
+  box->GetMaterialComponent()->AddNewMaterial("Wooden",
+    "../../data/textures/container.png", "",
+    "../../data/textures/container_specular.png",
+    "", "", "",
+    glm::vec3(0.5f),
+    glm::vec3(0.2f),
+    glm::vec3(1.0f),
+    1.0f, 0.0f, 0.0f, 0);
 
-  int num_cubes = 4;
+
+  /*int num_cubes = 4;
   for (int i = 0; i < num_cubes; i++) {
     for (int j = 0; j < num_cubes; j++) {
       for (int z = 0; z < num_cubes; z++) {
@@ -386,7 +405,7 @@ void OpenGLEngine::Prepare(){
         cubes.push_back(entity);
       }
     }
-  }
+  }*/
 
   std::shared_ptr<Entity> entity2 = OpenGLEngine::Engine::Core::entity_manager_->CreateEntity();
   entity2->AddMeshComponent();
@@ -403,11 +422,23 @@ void OpenGLEngine::Prepare(){
   entity3->AddMeshComponent();
   entity3->AddRenderComponent();
   entity3->AddTransformComponent();
-  entity3->GetTransformComponent()->SetPosition(glm::vec3(0.0f, -30.0f, 0.0f));
+  entity3->GetTransformComponent()->SetPosition(glm::vec3(-40.0f, 0.0f, 0.0f));
   //entity3->GetTransformComponent()->SetRotation(glm::vec3(-90.0f, 90.0f, 90.0f));
   entity3->GetTransformComponent()->SetScale(glm::vec3(11.0f, 11.0f, 11.0f));
   entity3->AddMaterialComponent();
-  entity3->GetMeshComponent()->LoadOBJ("../../data/models/Echidna/Echidna.obj", "../../data/models/Echidna/");
+  entity3->GetMeshComponent()->LoadOBJ("../../data/models/Mech/Low.obj");
+  entity3->GetMaterialComponent()->AddNewMaterial("Mech",
+    "../../data/models/Mech/textures/Low_Mech1_BaseColor.png",
+    "../../data/models/Mech/textures/Low_Mech1_Metallic.png",
+    "../../data/models/Mech/textures/Low_Mech1_Roughness.png",
+    "",
+    "",
+    "../../data/models/Mech/textures/Low_Mech1_Normal.png",
+    glm::vec3(0.5f),
+    glm::vec3(0.2f),
+    glm::vec3(1.0f),
+    1.0f, 0.0f, 0.0f, 0);
+
 
   std::shared_ptr<Entity> entity4 = OpenGLEngine::Engine::Core::entity_manager_->CreateEntity();
   entity4->AddMeshComponent();
@@ -436,13 +467,20 @@ void OpenGLEngine::Prepare(){
 
 void OpenGLEngine::Update() {
   
-  for (auto cube : cubes) {
-    glm::vec3 rotation = cube->GetTransformComponent()->GetRotation();
-    rotation.x += 1.0f;
-    rotation.y += 1.0f;
-    rotation.z += 1.0f;
-    cube->GetTransformComponent()->SetRotation(rotation);
-  }
+  //for (auto cube : cubes) {
+  //  glm::vec3 rotation = cube->GetTransformComponent()->GetRotation();
+  //  rotation.x += 1.0f;
+  //  rotation.y += 1.0f;
+  //  rotation.z += 1.0f;
+  //  cube->GetTransformComponent()->SetRotation(rotation);
+  //}
+
+  glm::vec3 box_rot = box->GetTransformComponent()->GetRotation();
+  box_rot.x += 0.1f;
+  box_rot.y += 0.3f;
+  box_rot.z += 0.5f;
+  box->GetTransformComponent()->SetRotation(box_rot);
+
 
   glm::vec3 rotation = earth->GetTransformComponent()->GetRotation();
   rotation.y += 0.3f;
